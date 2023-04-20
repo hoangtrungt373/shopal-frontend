c8a20749c3c673b6bbfccf622facc1d395b522ad
ToHoangTrung <37842252+ToHoangTrung@users.noreply.github.com>
2023-04-20 08:49:27 +0700

    config project

--- src/material/page/customer/productdetail/CustomerProductDetail.tsx
+++ src/material/page/customer/productdetail/CustomerProductDetail.tsx
@@ -4,7 +4,16 @@ import {ProductDetail} from "../../../model/ProductDetail";
 import {useHistory, useParams} from "react-router-dom";
 import {getProductDetailApi} from "../../../service/product.service";
 import {ExceptionResponse} from "../../../model/exception/ExceptionResponse";
-import {createTheme} from "@mui/material/styles";
+import {createTheme, ThemeProvider} from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import Container from "@mui/material/Container";
+import Header from "../home/Header";
+import MainFeaturedPost from "../home/MainFeaturedPost";
+import Grid from "@mui/material/Grid";
+import FeaturedPost from "../home/FeaturedPost";
+import Main from "../home/Main";
+import Sidebar from "../home/Sidebar";
+import Footer from "../home/Sidebar";
 
 interface RouteParams {
     productId: any;
@@ -38,9 +47,33 @@ const CustomerProductDetail = () => {
 
     if (productDetail) {
         return (
-            <div>
-                Product detail name = {productDetail.productName}
-            </div>
+            <ThemeProvider theme={theme}>
+                <CssBaseline/>
+                <Container maxWidth="lg">
+                    <Header title="Blog" sections={sections}/>
+                    <main>
+                        <MainFeaturedPost post={mainFeaturedPost}/>
+                        <Grid container spacing={4}>
+                            {featuredPosts.map((post) => (
+                                <FeaturedPost key={post.title} post={post}/>
+                            ))}
+                        </Grid>
+                        <Grid container spacing={5} sx={{mt: 3}}>
+                            <Main title="From the firehose" posts={posts}/>
+                            <Sidebar
+                                title={sidebar.title}
+                                description={sidebar.description}
+                                // archives={sidebar.archives}
+                                // social={sidebar.social}
+                            />
+                        </Grid>
+                    </main>
+                </Container>
+                <Footer
+                    title="Footer"
+                    description="Something here to give the footer a purpose!"
+                />
+            </ThemeProvider>
         );
     } else {
         return (
